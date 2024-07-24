1b684371abb132eb3e98a6f5b75734ac9859c385
igmlom <igmlom210@gmail.com>
2024-07-25 01:07:20 +0300

    multi accounts, with schedule

---
+++ accounts.py
@@ -0,0 +1,13 @@
+accounts = [
+    {
+        "username": "APEX_85123",
+        "password": "ARkh@57@Hj@#",
+        "email": "yybarhen@gmail.com"
+    },
+    {
+        "username": "APEX_100539",
+        "password": "A$@V6E#g3X#G",
+        # "email": "elchana1234@gmail.com"
+        "email": "yehonatan11barhen@gmail.com"
+    }
+]
